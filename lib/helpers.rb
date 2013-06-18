4266ef75c6a1cc048d8f2d5620ac05f64b5fcb6e
Andrew Griffith <griffithac@gamil.com>
2013-06-17 18:10:27 -0700

    add files

---
+++ lib/helpers.rb
@@ -0,0 +1,9 @@
+module Helpers
+
+  def number_with_delimiter(number)
+    parts = number.to_s.to_str.split('.')
+    parts[0].gsub!(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1#{','}")
+    parts.join('.')
+  end
+
+end
